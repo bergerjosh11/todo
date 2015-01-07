c34dd24deba8b3cc34dadec876a4706fc01ca0f4
bergerjosh11 <bergerjosh11@gmail.com>
2015-01-07 18:59:22 -0500

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# db/seeds.rb
+
+Todo.create!(title: 'shopping list', notes: 'jeans, tshirts, deoderant')
+Todo.create!(title: 'dentist appointment')
+Todo.create!(title: 'register for the 5k', notes: 'fill out the paperwork for the peachtree roadrace')
+Todo.create!(title: 'take the car to the mechanics', notes: 'The check engine light is on in the Rav4')
+Todo.create!(title: 'dog groomers', notes: 'Take Bandit and Jasmine to the groomers on Wednesday the 23rd')
